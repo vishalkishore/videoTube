e29e31b42a902da0b6fb581d7c1166f54e8bd7fb
kishore-das <vishal.turing@gmail.com>
2023-12-22 22:39:56 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 
 dotenv.config({
     path:"./.env"
 });
 
-connectDB();
+connectDB()
+.then( () => {
+    app.on("error", (error)=>{
+        console.error(`ERROR EXPRESS LOADING : ${error}`);
+        throw error;
+    });
+    const servingPORT =process.env.PORT || 8000;
+    app.listen( servingPORT, ()=>{
+        console.log(`Listening on PORT ${servingPORT}`);
+    })
+})
+.catch((error)=>{
+    console.error(`ERROR  INITALISATION: ${error}`);
+    throw error;
+});
